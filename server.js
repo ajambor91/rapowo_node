cb9e8e73bcc1f7fb663d82788d56fb61b096c909
Adam <adam91mko@gmail.com>
2020-04-28 20:23:46 +0200

    ws fix

--- server.js
+++ server.js
@@ -16,12 +16,28 @@ wsServer.on('connection', ws => {
     ws.send(JSON.stringify({status: true, data: 'init'}));
     ws.on('message', (msg)=> {
         const JSONMsg = JSON.parse(msg);
+        if(JSONMsg.type === 'close'){
+            return;
+        }
         const collLength = socketCollection.wsCollection.collection.length;
         for (let i = 0; i< collLength; i++){
             socketCollection.wsCollection.collection.push({userId: JSONMsg.userId, ws: ws});
         }
         if(collLength === 0){
             socketCollection.wsCollection.collection.push({userId: JSONMsg.userId, ws: ws});
+        }
+    })
+    ws.on('message',msg => {
+        const JSONmsg = JSON.parse(msg);
+        if(JSONmsg.type === 'init'){
+            return;
+        }
+        const collLength = socketCollection.wsCollection.collection.length;
+        for (let i = 0; i< collLength;i++){
+            if(socketCollection.wsCollection.collection[i].userId === JSONmsg.data){
+                socketCollection.wsCollection.collection.splice(i,1);
+                return;
+            }
         }
     })
 
